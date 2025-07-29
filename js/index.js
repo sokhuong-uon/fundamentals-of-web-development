f6c68cc79bed83d88ded85098903fd0e8f85cb0e
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-07-29 08:39:54 +0700

    feat: introduction to javascript

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// variables
+const name = "Jonh"
+let age = 25
+
+const pets = ["dog", 'cats', 'pig']
+const person = {
+  name: "Jonh",
+  age: 25
+}
+
+
+// DOM Manipulation
+const button = document.getElementById("first-button") 
+const aGroupOfElement = document.getElementsByClassName("papa")
+console.log(aGroupOfElement)
+const paragraph = document.getElementById("first-div") 
+console.log("this is the button:",button)
+
+button.addEventListener("pointerenter", (mouseEvent) => {
+ paragraph.innerText = "entered";
+ console.log(paragraph)
+})
+
+button.addEventListener("pointerleave", (mouseEvent) => {
+ paragraph.innerText = "left";
+ console.log(paragraph)
+})
+
+button.addEventListener("click", () => {
+  const age = prompt("Enter your age:")
+  console.log("User entered age (string):", age)
+
+  const ageInt = parseFloat(age)
+  console.log("converted age (number):", ageInt)
+})
