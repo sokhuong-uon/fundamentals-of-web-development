90b8fff6c2eef9e7cff6f77e3fd14ffcf453ef3b
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-07-30 16:52:29 +0700

    feat: simple todo list

--- js/index.js
+++ js/index.js
@@ -1,36 +1,24 @@
-// variables
-const name = "Jonh"
-let age = 25
-var dob = '2025/04/01'
+const form = document.getElementById('todo-form')
+const input = document.getElementById('new-todo')
+const todoList = document.getElementById('todos')
+console.log("todos list element: ",todoList)
 
-const pets = ["dog", 'cats', 'pig']
-const person = {
-  name: "Jonh",
-  age: 25
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newTodoContent = input.value;
+
+  // Create li element
+  const li = document.createElement('li')
+
+  /**
+   * Add new todo content to the li element
+   */
+  li.innerText = newTodoContent
+
+  // Add li to ul (todo list)
+  todoList.append(li)
+  
+  // Clear input value
+  input.value = ''
 }
-
-
-// DOM Manipulation
-const button = document.getElementById("first-button") 
-const aGroupOfElement = document.getElementsByClassName("papa")
-console.log(aGroupOfElement)
-const paragraph = document.getElementById("first-div") 
-console.log("this is the button:",button)
-
-button.addEventListener("pointerenter", (mouseEvent) => {
- paragraph.innerText = "entered";
- console.log(paragraph)
-})
-
-button.addEventListener("pointerleave", (mouseEvent) => {
- paragraph.innerText = "left";
- console.log(paragraph)
-})
-
-button.addEventListener("click", () => {
-  const age = prompt("Enter your age:")
-  console.log("User entered age (string):", age)
-
-  const ageInt = parseFloat(age)
-  console.log("converted age (number):", ageInt)
-})
