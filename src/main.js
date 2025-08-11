24933aaa902f9cd66e7bab4dcc826d8aa5a3caec
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-08-11 21:38:41 +0700

    chore: remove sample input and console.log

--- src/main.js
+++ src/main.js
@@ -3,18 +3,13 @@ const todoTaskContainer = document.getElementById("todo-tasks-container");
 
 const newTodoForm = document.getElementById("new-todo-form");
 
-addTaskButton.addEventListener("click", (event) => {
-  todoTaskContainer.dataset;
-  console.log(todoTaskContainer.dataset.showForm);
+addTaskButton.addEventListener("click", () => {
   todoTaskContainer.dataset.showForm = true;
-  console.log(todoTaskContainer.dataset.showForm);
 });
 
 newTodoForm.onsubmit = (event) => {
   event.preventDefault();
-  console.log(event);
   const formData = new FormData(newTodoForm);
 
   const inputData = Object.fromEntries(formData.entries());
-  console.log(inputData);
 };
