ee58bd91c053cd3af021986d7ea1f8a8ee62ff12
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-08-13 16:38:55 +0700

    feat: add todo item using HTML template

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const addTaskButton = document.getElementById("add-task-button");
 const todoTaskContainer = document.getElementById("todo-tasks-container");
 
+const todoList = todoTaskContainer.querySelector(".list");
+
 const newTodoForm = document.getElementById("new-todo-form");
 
 addTaskButton.addEventListener("click", () => {
@@ -12,4 +14,14 @@ newTodoForm.onsubmit = (event) => {
   const formData = new FormData(newTodoForm);
 
   const inputData = Object.fromEntries(formData.entries());
+  console.log(inputData);
+
+  const todoItemTemplate = document.getElementById("todo-item-template");
+  const list = todoItemTemplate.content.cloneNode(true);
+  const todoItemText = list.querySelector(".todo-item-text");
+  todoItemText.innerText = inputData["new-todo"];
+  console.log(todoItemText);
+  todoList.appendChild(list);
+
+  newTodoForm.reset();
 };
